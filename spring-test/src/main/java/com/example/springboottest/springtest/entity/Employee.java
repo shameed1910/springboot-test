c38270a999455e00deba9946266254eb44a14b3d
shameed1910 <shameed1910@gmail.com>
2021-02-10 17:24:14 +0530

    Added employee entity

---
+++ spring-test/src/main/java/com/example/springboottest/springtest/entity/Employee.java
@@ -0,0 +1,35 @@
+package com.example.springboottest.springtest.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table
+
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)	
+	
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
